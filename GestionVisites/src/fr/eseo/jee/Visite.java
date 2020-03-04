dde1678b76fe4a793a8e4b1837e0190d9b99e049
lubuntu <lubuntu@lubuntu-VirtualBox>
2020-03-04 09:42:19 +0100

    Finalisation classe Visite

--- GestionVisites/src/fr/eseo/jee/Visite.java
+++ GestionVisites/src/fr/eseo/jee/Visite.java
@@ -2,4 +2,44 @@ package fr.eseo.jee;
 
 public class Visite {
 
+	// Attributs
+	private String codeVisite;
+	private String typeVisite;
+	private String ville;
+	private String dateVisite;
+	private float prix;
+	
+	// Getters & Setters
+	
+	public String getCodeVisite() {
+		return this.codeVisite;
+	}
+	public void setCodeVisite(String codeVisite) {
+		this.codeVisite = codeVisite;
+	}
+	public String getTypeVisite() {
+		return this.typeVisite;
+	}
+	public void setTypeVisite(String typeVisite) {
+		this.typeVisite = typeVisite;
+	}
+	public String getVille() {
+		return this.ville;
+	}
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+	public String getDateVisite() {
+		return this.dateVisite;
+	}
+	public void setDateVisite(String dateVisite) {
+		this.dateVisite = dateVisite;
+	}
+	public float getPrix() {
+		return this.prix;
+	}
+	public void setPrix(float prix) {
+		this.prix = prix;
+	}
+	
 }
