bd51e2f59fe1b70fdbb0274ebcf65c1269d36003
clement <clement@clement-hp-spectre-x360>
2020-03-04 09:58:18 +0100

    Création du fichier GestionVisites

---
+++ GestionVisites/src/fr/eseo/jee/GestionVisites.java
@@ -0,0 +1,27 @@
+package fr.eseo.jee;
+
+import java.util.List;
+
+public class GestionVisites {
+
+	/**
+	 * Prend une visite en paramètre
+	 * @param uneVisiste
+	 * @return List<Visite>
+	 */
+	public List<Visite> trouverVisite(Visite uneVisiste) {
+
+	}
+
+	public String reserverVisite(ReservationVisite uneReservation) {
+
+	}
+
+	public String payerVisite(String codeReservation) {
+
+	}
+
+	public boolean annulerVisite(String codeReservation) {
+
+	}
+}
