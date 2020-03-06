449bd6f953a31e6da41f72a5b0db6af3c34980e0
lubuntu <lubuntu@lubuntu-VirtualBox>
2020-03-06 14:04:19 +0100

    First test

---
+++ GestionVisites/test/fr/eseo/jee/TestGestionVisites.java
@@ -0,0 +1,29 @@
+package fr.eseo.jee;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestGestionVisites {
+
+	@Test
+	public void testTrouverVisite() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testReserverVisite() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPayerVisite() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAnnulerVisite() {
+		fail("Not yet implemented");
+	}
+
+}
