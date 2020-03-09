e3ca6692fe92ee92d911e9ea79120b57cab30982
clement <clement@clement-hp-spectre-x360>
2020-03-09 17:19:15 +0100

    Tests pour la méthode trouverVisites

--- GestionVisites/test/fr/eseo/jee/AllTests.java
+++ GestionVisites/test/fr/eseo/jee/AllTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ TestAnnulerVisite.class, TestPayerVisite.class, TestReserverVisite.class })
+@SuiteClasses({ TestAnnulerVisite.class, TestPayerVisite.class, TestReserverVisite.class, TestTrouverVisites.class })
 public class AllTests {
 	static final String DB_ADRESSE = "localhost";
 	// static final String DB_ADRESSE = "192.168.4.197";
---
+++ GestionVisites/test/fr/eseo/jee/TestTrouverVisites.java
@@ -0,0 +1,133 @@
+package fr.eseo.jee;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.mysql.cj.jdbc.Driver;
+
+public class TestTrouverVisites {
+
+	@BeforeClass
+	public static void creationVisitesTest() {
+		try {
+			DriverManager.registerDriver(new Driver());
+			Connection conn = DriverManager.getConnection(
+					"jdbc:mysql://" + AllTests.DB_ADRESSE + "/GestionnaireVisites", AllTests.USERNAME,
+					AllTests.PASSWORD);
+
+			Statement stmt = conn.createStatement();
+
+			String query = "INSERT INTO Visites(typeVisite, ville, dateVisite, prixVisite, codeVisite) VALUES ('test', 'angersTest', '2020-03-25', '29.99', 'teangers250320')";
+			stmt.executeUpdate(query);
+			query = "INSERT INTO Visites(typeVisite, ville, dateVisite, prixVisite, codeVisite) VALUES ('test2', 'angersTest', '2020-03-26', '39.99', 'teangers260320')";
+			stmt.executeUpdate(query);
+
+			stmt.close();
+			conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@AfterClass
+	public static void supressionVisiteTest() {
+		try {
+			DriverManager.registerDriver(new Driver());
+			Connection conn = DriverManager.getConnection(
+					"jdbc:mysql://" + AllTests.DB_ADRESSE + "/GestionnaireVisites", AllTests.USERNAME,
+					AllTests.PASSWORD);
+
+			Statement stmt = conn.createStatement();
+
+			String query = "DELETE FROM Visites WHERE codeVisite = 'teangers250320'";
+			stmt.executeUpdate(query);
+
+			query = "DELETE FROM Visites WHERE codeVisite = 'teangers260320'";
+			stmt.executeUpdate(query);
+
+			stmt.close();
+			conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void testTrouverVisiteNExistePas() {
+		Visite visiteTest = new Visite();
+		visiteTest.setCodeVisite("nexistepas");
+		GestionVisites gestion = new GestionVisites();
+		List<Visite> listeVisites = gestion.trouverVisite(visiteTest);
+
+		Assert.assertTrue("Il n'y a pas de visites", listeVisites.isEmpty());
+	}
+
+	@Test
+	public void testTrouverVisiteExisteTypeVisite() {
+		Visite visiteTest = new Visite();
+		visiteTest.setTypeVisite("test");
+		GestionVisites gestion = new GestionVisites();
+		List<Visite> listeVisites = gestion.trouverVisite(visiteTest);
+
+		Assert.assertEquals("Il y a une visite", 1, listeVisites.size());
+		Assert.assertEquals("Il y a une visite du type 'test'", "test", listeVisites.get(0).getTypeVisite());
+	}
+
+	@Test
+	public void testTrouverVisiteExiste2Villes() {
+		Visite visiteTest = new Visite();
+		visiteTest.setVille("angers");
+		GestionVisites gestion = new GestionVisites();
+		List<Visite> listeVisites = gestion.trouverVisite(visiteTest);
+
+		Assert.assertEquals("Il y a une visite", 2, listeVisites.size());
+		Assert.assertEquals("Il y a une visite dans la ville 'angersTest'", "angersTest",
+				listeVisites.get(0).getVille());
+		Assert.assertEquals("Il y a une visite dans la ville 'angersTest'", "angersTest",
+				listeVisites.get(1).getVille());
+	}
+
+	@Test
+	public void testTrouverVisiteExisteDate() {
+		Visite visiteTest = new Visite();
+		visiteTest.setDateVisite("2020-03-25");
+		GestionVisites gestion = new GestionVisites();
+		List<Visite> listeVisites = gestion.trouverVisite(visiteTest);
+
+		Assert.assertEquals("Il y a une visite", 1, listeVisites.size());
+		Assert.assertEquals("Il y a une visite à la date '2020-03-25'", "2020-03-25",
+				listeVisites.get(0).getDateVisite());
+	}
+
+	@Test
+	public void testTrouverVisiteExistePrix() {
+		Visite visiteTest = new Visite();
+		visiteTest.setPrix(29.99);
+		GestionVisites gestion = new GestionVisites();
+		List<Visite> listeVisites = gestion.trouverVisite(visiteTest);
+
+		Assert.assertEquals("Il y a une visite", 1, listeVisites.size());
+		Assert.assertEquals("Il y a une visite au prix '29.99'", 29.99, listeVisites.get(0).getPrix(), 0);
+	}
+
+	@Test
+	public void testTrouverVisiteExisteCodeVisite() {
+		Visite visiteTest = new Visite();
+		visiteTest.setCodeVisite("teangers250320");
+		GestionVisites gestion = new GestionVisites();
+		List<Visite> listeVisites = gestion.trouverVisite(visiteTest);
+
+		Assert.assertEquals("Il y a une visite", 1, listeVisites.size());
+		Assert.assertEquals("Il y a une visite avec le code 'teangers250320'", "teangers250320",
+				listeVisites.get(0).getCodeVisite());
+	}
+
+}
